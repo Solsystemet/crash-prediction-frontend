e9d0ccb20a8ab2fd366b950d185e810ae4e7c082
Nicolaj Blach Jensen <me@nicolaj-blach.dev>
2026-04-08 12:00:12 +0200

    feat: setup project

---
+++ src/components/Map.tsx
@@ -0,0 +1,26 @@
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet"
+import L from 'leaflet';
+
+// Fix for default marker icons in React Leaflet
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
+  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+});
+
+export default function Map() {
+  return (
+    <MapContainer center={[41.8781, -87.6298]} zoom={11} scrollWheelZoom={false} className="h-full w-full">
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[41.8781, -87.6298]}>
+        <Popup>
+          Chicago, IL
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+}
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,6 +2,7 @@ import { HeadContent, Scripts, createRootRoute } from "@tanstack/react-router"
 import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools"
 import { TanStackDevtools } from "@tanstack/react-devtools"
 
+import "leaflet/dist/leaflet.css"
 import appCss from "../styles.css?url"
 
 export const Route = createRootRoute({
